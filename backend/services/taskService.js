7307785875136f949b6f6f99e93222d9045faed9
Amir Nesru <amirnesru054@gmail.com>
2026-07-31 22:42:28 +0300

    feat: implement task service CRUD operations

---
+++ backend/services/taskService.js
@@ -0,0 +1,42 @@
+const taskData = require("../data/taskData");
+
+function getAllTasks() {
+  return taskData;
+}
+
+function getTask(id) {
+  return taskData.find((task) => id === task.id);
+}
+
+function createNewTask(newTask) {
+  taskData.push(newTask);
+  return newTask;
+}
+
+function deleteTask(id) {
+  const index = taskData.findIndex((task) => task.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [deletedTask] = taskData.splice(index, 1);
+  return deletedTask;
+}
+
+function updateTask(id, updatedData) {
+  const task = taskData.find((task) => task.id === id);
+  if (!task) {
+    return null;
+  }
+
+  Object.assign(task, updatedData);
+
+  return task;
+}
+
+module.exports = {
+  getAllTasks,
+  getTask,
+  createNewTask,
+  updateTask,
+  deleteTask,
+};
