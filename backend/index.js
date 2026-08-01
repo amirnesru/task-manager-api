7d0ffd807fefe5114c5f0f831565c4212f7f90b0
Amir Nesru <amirnesru054@gmail.com>
2026-08-01 17:14:19 +0300

    feat: configure express server and middleware

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const taskRoutes = require("./routes/taskRoutes");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Task Manager API");
-});
+app.use(express.json());
+app.use(cors());
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.use("/api/tasks", taskRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
